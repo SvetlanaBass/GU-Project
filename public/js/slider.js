f8a98b85f0087c98e4f00d6bd589ec2f7ebcffe1
Svetlana <svetabass@gmail.com>
2018-02-02 22:46:21 +0200

    Повторяющиеся в функциях слайдера переменные, вынесены отдельно

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,13 +1,14 @@
-var slideIndex = 0;
-var repeat;
+let slideIndex = 0;
+let repeat;
+let slides = document.querySelectorAll(".slide");
+let bg = document.querySelector(".slider");
+let dot = document.querySelectorAll(".dot");
+let bgcolor = ["#e8e8e8", "#0e0f14", "#f7f7f7"];
+
 showSlides();
 
 function showSlides() {
-    var slides = document.querySelectorAll(".slide");
-    var bg = document.querySelector(".slider");
-    var dot = document.querySelectorAll(".dot");
-    var bgcolor = ["#e8e8e8", "#0e0f14", "#f7f7f7"];
-    for (var i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         dot[i].style.backgroundColor = "transparent";
         slides[i].style.display = "none";
     }
@@ -21,14 +22,9 @@ function showSlides() {
     repeat = setTimeout(showSlides, 3000);
 }
 
-
 function currentSlide(n) {
     clearTimeout(repeat);
-    var slides = document.querySelectorAll(".slide");
-    var bg = document.querySelector(".slider");
-    var dot = document.querySelectorAll(".dot");
-    var bgcolor = ["#e8e8e8", "#0e0f14", "#f7f7f7"];
-    for (var i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         dot[i].style.backgroundColor = "transparent";
         slides[i].style.display = "none";
     }
